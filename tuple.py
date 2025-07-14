68527a352986fb02829e74d29c3cd447384bbc36
Komal Salman <komalsalman62@gmail.com>
2025-07-14 17:53:37 +0500

    Add files via upload

---
+++ tuple.py
@@ -0,0 +1,20 @@
+tuple1=("cake","cookies","pizza","burger","donut")
+tuple2=(1,2,3,4,5)
+tuple3=("sprikles",7,"sixty",8)
+tuple4=()  #it is called none/empty tuple
+tuple5=(50,) #in single value we add comma 
+
+#print(tuple1,tuple2,tuple3)
+tuplemix=tuple1+tuple2+tuple3
+# print(tuplemix)
+# print(tuple4)
+# print(tuple5)
+# print(tuple1[0]) #access index value 
+# print(tuple1[-2])
+
+newtuple=tuple1+(1,3) #we cant insert in tuple string only int
+# print(newtuple)
+
+mytuple=tuple3*7 #if we repeat a tuple it has to be multiply
+# print(mytuple)
+
